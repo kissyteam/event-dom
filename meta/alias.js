e782e6833d7775ec4f3abae49ee70c64e5b297b3
yiminghe <yiminghe@gmail.com>
2014-09-16 14:38:48 +0800

    optimize alias

--- meta/alias.js
+++ meta/alias.js
@@ -1,19 +1,9 @@
-(function () {
-    function init(UA, Feature) {
-        modulex.config('alias', {
-            'event-dom': [
-                'event-dom/base',
-                Feature.isHashChangeSupported() ? '' : 'event-dom/hashchange',
-                    UA.ieMode < 9 ? 'event-dom/ie' : '',
-                Feature.isInputEventSupported() ? '' : 'event-dom/input',
-                UA.ie ? '' : 'event-dom/focusin'
-            ]
-        });
-    }
-
-    if (typeof UA !== 'undefined') {
-        init(UA, Feature);
-    } else {
-        modulex.use(['ua', 'feature'], init);
-    }
-})();
+modulex.config('alias', {
+    'event-dom': [
+        'event-dom/base',
+        Feature.isHashChangeSupported() ? '' : 'event-dom/hashchange',
+            UA.ieMode < 9 ? 'event-dom/ie' : '',
+        Feature.isInputEventSupported() ? '' : 'event-dom/input',
+        UA.ie ? '' : 'event-dom/focusin'
+    ]
+});
